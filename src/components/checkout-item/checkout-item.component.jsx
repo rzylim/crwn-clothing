5c04a4e5e716df48af500eb24705bbfc48c5be88
Zhen Yi Lim <zhen.yi@outlook.com>
2020-02-22 00:27:06 +0800

    switch app to use contextAPI from redux

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,15 +1,11 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useContext } from "react";
 
-import {
-  addItem,
-  removeItem,
-  clearItemFromCart
-} from "../../redux/cart/cart.actions";
+import { CartContext } from "../../providers/cart/cart.provider";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({ cartItem, addItem, removeItem, clearItem }) => {
+const CheckoutItem = ({ cartItem }) => {
+  const { addItem, removeItem, clearItemFromCart } = useContext(CartContext);
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
@@ -27,17 +23,14 @@ const CheckoutItem = ({ cartItem, addItem, removeItem, clearItem }) => {
         </div>
       </span>
       <span className="price">{price}</span>
-      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+      <div
+        className="remove-button"
+        onClick={() => clearItemFromCart(cartItem)}
+      >
         &#10005;
       </div>
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  addItem: item => dispatch(addItem(item)),
-  removeItem: item => dispatch(removeItem(item)),
-  clearItem: item => dispatch(clearItemFromCart(item))
-});
-
-export default connect(null, mapDispatchToProps)(CheckoutItem);
+export default CheckoutItem;
